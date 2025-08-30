4cf2ebdb13a2c93aa7f4dfcf0f2922ce15c0870f
Avi-123222 <dasavishek669@gmail.com>
2025-08-30 22:16:58 +0530

    first react code by using component babel

---
+++ components_babel/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  const a = 100;
+  const b = 20;
+  console.log(a+b);
+
+  return (
+    // <h1>a + b</h1>
+    // <h1>{a}</h1>
+    // <div>
+    // <h1>{a+b === 30 ? "React is easy" : "React is hard"}</h1>  
+    // <h2>Mentor:</h2>
+    // </div>
+
+      // <React.Fragment>
+      //   <h1>{a+b === 30 ? "React is easy" : "React is difficult"}</h1>
+      //   <h2>Mentor: <b>Sai Pranab</b></h2>
+      // </React.Fragment> //we can write like this or 
+
+      <>
+        <h1>{a+b === 30 ? "React is easy" : "React is difficult"}</h1>
+       <h2>Mentor: <b>Sai Pranab</b></h2>
+     </>
+    )
+}
+
+const val = App()
+console.log('value is ', val)
+
+export default App
+
+// function app() {
+//   return <h1>Java Technocrat</h1>
+// }
+
+
+
+
+
+
+
+
+
+// normal html code which returns javascript like code is called component
+// DEFINITION-->>'normal javascript fnction which returns the react element is called COMPONENT'
