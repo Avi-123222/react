d2c5414579c8671f664decf88f1736e1b9e30080
Avi-123222 <dasavishek669@gmail.com>
2025-09-23 08:41:09 +0530

    expense calculator made the table dynamic and some syntax of typescript

---
+++ expense-calculator/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import ExpenseForm from './components/ExpenseForm'
+import ExpenseTable from './components/ExpenseTable'
+import type { Expense } from './model'
+import expenseData from './ExpenseData'
+
+function App() {
+  const [expenses, setExpenses] = useState<Expense[]>(expenseData);
+
+  return (
+    <>
+      <main>
+        <h1>Track Your Expense</h1>
+        <div className="expense-tracker">
+          <ExpenseForm setExpenses={setExpenses}/>
+          <ExpenseTable expenses={expenses}/>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ expense-calculator/src/components/ExpenseForm.tsx
@@ -0,0 +1,53 @@
+
+import type { FormEvent } from "react"
+import type { Expense } from "../model/index";
+
+interface ExpenseFormProps {
+  setExpenses: React.Dispatch<React.SetStateAction<Expense[]>>;
+}
+
+function ExpenseForm({ setExpenses }: ExpenseFormProps) {
+  function handleSubmit(e: FormEvent): void {
+    e.preventDefault()
+
+    const formData: FormData = new FormData(e.target as HTMLFormElement);
+    const expense: any = {}
+    for (const [key, value] of formData.entries()) {
+      // console.log(key, '->', value)
+      expense[key] = value;
+    }
+
+    setExpenses(expenses => ([...expenses, { ...expense, id: crypto.randomUUID() }]));
+
+    (e.target as HTMLFormElement).reset();
+  }
+
+  return (
+    <>
+      <form className="expense-form" onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="title">Title</label>
+          <input id="title" name="title" />
+        </div>
+        <div className="input-container">
+          <label htmlFor="category">Category</label>
+          <select id='category' name='category'>
+            <option hidden>Select Category</option>
+            <option value="Grocery">Grocery</option>
+            <option value="Clothes">Clothes</option>
+            <option value="Bills">Bills</option>
+            <option value="Education">Education</option>
+            <option value="Medicine">Medicine</option>
+          </select>
+        </div>
+        <div className="input-container">
+          <label htmlFor="amount">Amount</label>
+          <input id="amount" name="amount" />
+        </div>
+        <button className="add-btn">Add</button>
+      </form>
+    </>
+  )
+}
+
+export default ExpenseForm
